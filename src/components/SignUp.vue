affeb5bec5cfd7d5609e67683243c88f949b6f20
Iskander <iskander1998@list.ru>
2024-02-10 17:15:29 +0300

    upgrading signup

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,16 +1,38 @@
 <template>
   <img class="logo" src="../assets/logo.png" />
-  <h1>Sign Up</h1>
+  <h1 class="title">Регистрация в TodoApp</h1>
   <div class="register">
-    <input type="text" placeholder="Enter Name" />
-    <input type="text" placeholder="Enter Email" />
-    <input type="password" placeholder="Enter Password" />
-    <button>Sign Up</button>
+    <input type="text" v-model="name" placeholder="Введите Имя" />
+    <input type="text" v-model="email" placeholder="Введите ваш Email" />
+    <input type="password" v-model="password" placeholder="Введите пароль" />
+    <button @click="signUp">Зарегестрироваться</button>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        alert("sign-up-done");
+        localStorage.setItem("user-info", JSON.stringify(result.data));
+      }
+    },
+  },
 };
 </script>
 <style>
@@ -25,5 +47,17 @@ export default {
   margin-bottom: 30px;
   margin-right: auto;
   margin-left: auto;
+  border: 1px solid rgb(0, 0, 0);
+}
+.register button {
+  width: 320px;
+  height: 50px;
+  border: 1px solid black;
+  background: black;
+  color: white;
+  cursor: pointer;
+}
+.title {
+  color: black;
 }
 </style>
