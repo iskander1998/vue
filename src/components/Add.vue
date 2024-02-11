49d37a0ad906fec0003f3a21acf8d68015742f68
Iskander <iskander1998@list.ru>
2024-02-11 06:31:07 +0300

    add and update notes

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,19 +1,13 @@
 <template>
   <Header />
-  <h1>Hello {{ name }}, Welcome to home page</h1>
+  <h1>Добавить заметку</h1>
 </template>
 <script>
 import Header from "./Header.vue";
 export default {
-  name: "Home",
-  data() {
-    return {
-      name: "",
-    };
-  },
+  name: "Add",
   mounted() {
     let user = localStorage.getItem("user-info");
-    this.name = JSON.parse(user).name;
     if (!user) {
       this.$router.push({ name: "SignUp" });
     }
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Главная</router-link>
+    <router-link to="/Add">Создать новую заметку</router-link>
+    <router-link to="/Update">Изменить</router-link>
+    <a @click="logout" href="#">Выйти</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push({ name: "Login" });
+    },
+  },
+};
+</script>
+<style>
+.nav {
+  background: #333;
+  overflow: hidden;
+}
+.nav a {
+  float: left;
+  color: whitesmoke;
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 17px;
+  text-decoration: none;
+  margin-right: 5px;
+}
+.nav a:hover {
+  background: whitesmoke;
+  color: black;
+}
+</style>
