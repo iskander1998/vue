566061f138644f50c2dca923630fb9f99e8504a2
Валишин Искандер Айратович <IsAValishin@int.kpfu.ru>
2024-02-15 14:48:15 +0300

    update eslintconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
+    "linebreak-style": ["error", "windows"],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": 0,
